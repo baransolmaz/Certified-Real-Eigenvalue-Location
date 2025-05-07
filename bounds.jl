1871d15989f5508123ada898ab4a7b4eb73ca54d
Baran Solmaz <baransolmaz1361@hotmail.com>
2025-05-07 21:42:11 +0300

    ref added

--- bounds.jl
+++ bounds.jl
@@ -5,14 +5,22 @@ Bounds for Elgenvalues Using Traces*
     Hemy WoIkowicz
     George P. H. Styan
 =#
-#1.1
+#=1.1
+Ref[9,  p. 144]
+M.  Marcus  and  H. Mint, A Survey of Matrix Theory and Matrix Inequalities, 
+Prindle,  Weber &  Schmidt,  Boston, 1964.
+=#
 function compute_R_C(A::AbstractMatrix)
     R = maximum(sum(abs.(A), dims=2))
     C = maximum(sum(abs.(A), dims=1))
     return R, C
 end
 
-#1.2
+#=1.2
+Ref  [9,  p.  145]
+M.  Marcus  and  H. Mint, A Survey of Matrix Theory and Matrix Inequalities, 
+Prindle,  Weber &  Schmidt,  Boston, 1964.
+=#
 function brauer_inequality(lambda::Number, A::AbstractMatrix)
     return abs.(lambda) <= minimum(compute_R_C(A))
 end
@@ -22,11 +30,17 @@ function eq1_3(lambda::Number, A::AbstractMatrix)
     #TODO
 end
 
-#1.4
+#=1.4
+Ref  [1,  p.  133]
+A. R  Amir-Moez and A. L.  Fass, Elements of Linear  Spaces, Pergamon, Oxford, 1962
+=#
 function matrix_B(A::AbstractMatrix)
     return (1 / 2) * (A + adjoint(A))
 end
-#1.4
+#=1.4
+Ref  [1,  p.  133]
+A. R  Amir-Moez and A. L.  Fass, Elements of Linear  Spaces, Pergamon, Oxford, 1962
+=#
 function matrix_C(A::AbstractMatrix)
     return (1 / 2) * (A - adjoint(A)) / 1im
 end
@@ -45,13 +59,20 @@ end
 function eq1_7(A::AbstractMatrix)
     #TODO
 end
-#1.8
+#=1.8 S= Standart Deviation 
+[4,p. 2271
+F. A. Graybill, Zntroductionto Matrices with Applicutions in Statistics, Wadsworth, Belmont, Cal&, 196
+=#
 function s_square(A::AbstractMatrix)
     n,m= size(A) 
     return ((tr(A*A) - ((tr(A)^2))/n)/n)
 end
 
 #1.10
+#=
+H. Wolkowicz and G. P. H. Styan, Extensions of Samuelson’s inequality, Aw.
+stat. 33:143-144 (1979)
+=#
 function min_bound(A::AbstractMatrix)
     n, m = size(A)
     m = mean_m(A)
@@ -60,6 +81,10 @@ function min_bound(A::AbstractMatrix)
 end
 
 #1.10
+#=
+H. Wolkowicz and G. P. H. Styan, Extensions of Samuelson’s inequality, Aw.
+stat. 33:143-144 (1979)
+=#
 function max_bound(A::AbstractMatrix)
     n, m = size(A)
     m = mean_m(A)
@@ -68,6 +93,10 @@ function max_bound(A::AbstractMatrix)
 end
 
 #1.11
+#=
+H. Wolkowicz and G. P. H. Styan, Extensions of Samuelson’s inequality, Aw.
+stat. 33:143-144 (1979)
+=#
 function mean_m(A::AbstractMatrix)
     n, m = size(A)
     return tr(A)/n
